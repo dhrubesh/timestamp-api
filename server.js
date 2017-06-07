a106044d85869e9aa77eb50e144d024284a59e8f
dhrubesh <dhrubesh97@gmail.com>
2017-06-07 09:26:56 +0000

    unix code done

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ var PORT = process.env.PORT || 8080;
 
 app.get('/:timestamp', function (req, res) {
     var timestamp = req.param.timestamp;
-  res.send('Timestamp: '+timestamp)
+    res.json(getTimestampJSON(timestamp))
+//   res.send('Timestamp: '+timestamp)
 })
+
+function getTimestampJSON(timestamp){
+    
+    var results={
+        unix:null;
+        natural:null;
+    }
+    var date;
+    if(!isNAN(parseInt(timestamp))){
+        date = new Date(parseInt(timestamp))
+    }else{
+        date= new Date(timestamp)
+    }
+    if(!isNaN(date.getTime())){
+        result.unix = date.getTime()
+        result.natural  = null;
+    }
+    return result;
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
